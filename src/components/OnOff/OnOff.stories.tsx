b9038886c1b5a7e1d602bad267c11d10703625fc
nikitastas <nikitastas1694@gmail.com>
2024-09-14 23:54:44 +0300

    Added FirstStory in OnOff.stories.tsx.

---
+++ src/components/OnOff/OnOff.stories.tsx
@@ -0,0 +1,23 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import {OnOff} from './OnOff';
+import {useState} from 'react';
+
+// const meta: Meta<typeof Accordion> = {
+//     component: Accordion,
+// };
+// export default meta;
+
+export default {
+    component: OnOff
+}
+
+type Story = StoryObj<typeof OnOff>
+
+export const FirstStory: Story = {
+    args: {
+        on: true,
+        setOn: () => {}
+    }
+}
