d26e8025ffeac0127a712954f08cfe8921b3a1dd
nikitastas <nikitastas1694@gmail.com>
2024-10-07 03:04:39 +0300

    Added TrackValueOfUncontrolledInput and GetValueOfUncontrolledInputByButtonPress in input.stories.tsx

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,6 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import {Simulate} from 'react-dom/test-utils';
 import input = Simulate.input;
 
@@ -24,4 +24,28 @@ export default {
 
 export const UncontrolledInput = () => {
     return <input />
+}
+
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    return <><input onChange={(event) => {
+        setValue(event.currentTarget.value)
+    }
+    }/> - {value}</>
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+
+    return <><input ref={inputRef} /><button onClick={save}>Save</button> - actual value: {value}</>
+}
+
+export const ControlledInputWithFiexedValue = () => {
+    return <input value={'it-incubator.by'} />
 }
