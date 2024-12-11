650688d218cf47411f8a3758eb79fae762e1ac8e
nikitastas <nikitastas1694@gmail.com>
2024-12-11 22:48:07 +0300

    Completed useEffect setInterval 22.

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -28,3 +28,32 @@ export const Example1 = () => {
         <button onClick={() => setCounter(counter + 1)}>counter +</button>
     </>
 }
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+    const [time, setTime] = useState(new Date())
+    console.log('setTimeout example')
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setCounter(counter => counter + 1)
+        }, 1000)
+    }, [])
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+    }, [])
+
+    return <>
+        Hello, counter: {counter} - fake: {fake}
+        <div>
+            Clock: {`${time.getHours()}:${time.getMinutes()}:${time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds()}`}
+        </div>
+        {/*<button onClick={() => setFake(fake + 1)}>fake +</button>
+        <button onClick={() => setCounter(counter + 1)}>counter +</button>*/}
+    </>
+}
